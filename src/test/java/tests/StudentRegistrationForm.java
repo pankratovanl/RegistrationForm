2ee1e99cd43cddf20af11e8d2ee0a328140f8888
pankratovanl <nkvcxmfqqffi@mail.ru>
2025-02-02 23:42:09 +0500

    mentor's recommendations were used

--- src/test/java/tests/StudentRegistrationForm.java
+++ src/test/java/tests/StudentRegistrationForm.java
@@ -1,14 +1,14 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
-public class StudentRegistrationform {
+public class StudentRegistrationForm {
 
     @BeforeAll
     static void beforeAll() {
@@ -16,32 +16,49 @@ public class StudentRegistrationform {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
+        //Configuration.holdBrowserOpen = true;
+
     }
 
     @Test
-    void FillFormTest() {
+    void fillFormTest() {
 
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
 
         //заполнение формы
         $("#firstName").setValue("Yuriy");
         $("#lastName").setValue("Gagarin");
         $("#userEmail").setValue("gagarin@comp.ru");
-        $(byText("Male")).click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("0700000000");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue("2");
         $(".react-datepicker__year-select").selectOptionByValue("1934");
         $(".react-datepicker__day--009").click();
         $("#subjectsInput").setValue("Arts").pressEnter();
-        $(byText("Music")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
         $("#uploadPicture").uploadFromClasspath("img/cat.png");
         $("#currentAddress").setValue("Broadway,11");
         $("#react-select-3-input").setValue("Rajasthan").pressEnter();
         $("#react-select-4-input").setValue("Jaiselmer").pressEnter();
         $("#submit").click();
+
+        //проверка заполненных полей
+
+        $(".table-responsive").shouldHave(Condition.text("Yuriy Gagarin"),
+                (Condition.text("gagarin@comp.ru")),
+                (Condition.text("Male")),
+                (Condition.text("0700000000")),
+                (Condition.text("09 March,1934")),
+                (Condition.text("Arts")),
+                (Condition.text("Music")),
+                (Condition.text("cat.png")),
+                (Condition.text("Broadway,11")),
+                (Condition.text("Rajasthan Jaiselmer")));
+        $(".modal-footer").$("#closeLargeModal").click();
+
 
 
     }
